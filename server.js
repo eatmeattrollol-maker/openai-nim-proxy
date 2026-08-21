d2c20c98c76b0cae76b568ca1d8257133f7f438e
eatmeattrollol-maker <eatmeattrollol@gmail.com>
2026-08-21 13:52:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -212,7 +212,7 @@ function addModelSpecificSettings(body, model) {
         enable_thinking: false
       };
     } else {
-      body.reasoning_effort = "medium";
+      body.reasoning_effort = "high";
     }
 
     return;
